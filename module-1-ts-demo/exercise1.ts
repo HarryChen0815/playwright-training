2928b651596c2981637a6c2b77d37138014cf236
Qiyu Chen <54870292+HarryChen0815@users.noreply.github.com>
2024-04-05 14:02:17 +1300

    Create exercise1.ts

---
+++ module-1-ts-demo/exercise1.ts
@@ -0,0 +1,11 @@
+function calculateSquare(length: number): number {​
+
+    return length * length;
+}
+function calculateTriangleArea(base: number, height: number): number {​
+
+    return base * height / 2;
+}​
+  
+console.log(calculateSquare(15));
+console.log(calculateTriangleArea(10,14))
